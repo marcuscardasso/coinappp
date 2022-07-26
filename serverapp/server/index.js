a293c10d374e7358971ee196ad609aa96baf2e9f
marcuscardasso <107311702+marcuscardasso@users.noreply.github.com>
2022-07-26 15:56:52 +0100

    clean
    clean

--- serverapp/server/index.js
+++ serverapp/server/index.js
@@ -51,7 +51,15 @@ app.use(admin);
 app.use(user);
 app.use(contact);
 
-mongoose.connect('mongodb://db:27017/db', {
+server.listen(PORT, async (error) => {
+  if (error) {
+    return error;
+  }
+
+  return console.log(`server started on port here now ${PORT}`);
+});
+
+/*mongoose.connect('mongodb://db:27017/db', {
   //mongodb://db:27017/db =====> production
   //mongodb://127.0.0.1:27017/db ===> development
 
@@ -69,4 +77,4 @@ mongoose.connect('mongodb://db:27017/db', {
     
       return console.log(`server started on port here now ${PORT}`);
     });
-});
+});*/
